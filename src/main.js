c6f54b0ccb18753547a8c98bfac07ff161499211
Aliaksandr Bulko <aliaksandr.bulko@sitewerk.ch>
2023-03-16 14:37:16 +0400

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import './styles/main.scss'
 
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+import InitHelper from './lib/InitHelper'
+import './registerServiceWorker'
+
+Vue.config.productionTip = false
+
+window.__store = store
+
+InitHelper.init(function() {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+})
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: "My App",
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  },
+  publicPath: './',
+
+  chainWebpack: config => {
+    // Добавляем поддержку coffee script с обработчкой через babel
+    config.resolve.extensions.add('.coffee')
+
+    config.module
+      .rule('coffee')
+      .test(/\.coffee$/)
+      .use('babel-loader')
+      .loader('babel-loader').end()
+      .use('coffee-loader')
+      .loader('coffee-loader').end()
+
+  },
 });
